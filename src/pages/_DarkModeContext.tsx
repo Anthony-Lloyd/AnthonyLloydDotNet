7014982bec1052cd33c1d351a366b83274bcdffa
Anthony Lloyd <contact@anthonylloyd.net>
2023-11-08 22:44:40 +1100

    updated dark mode context

--- src/pages/_DarkModeContext.tsx
+++ src/pages/_DarkModeContext.tsx
@@ -1,46 +1,65 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
 
 interface DarkModeContextType {
-    isDarkMode: boolean;
-    toggleDarkMode: () => void;
+  isDarkMode: boolean;
+  toggleDarkMode: () => void;
 }
 
 const DarkModeContext = createContext<DarkModeContextType | undefined>(undefined);
 
 interface DarkModeProviderProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 export const DarkModeProvider: React.FC<DarkModeProviderProps> = ({ children }) => {
-    const [isDarkMode, setIsDarkMode] = useState(() => {
-        const savedTheme = localStorage.getItem('dark-mode');
-        return savedTheme === 'true';
-    });
+  // First, we try to get the saved theme from localStorage or fall back to the system preference
+  const [isDarkMode, setIsDarkMode] = useState(() => {
+    const savedTheme = localStorage.getItem('dark-mode');
+    if (savedTheme !== null) {
+      return savedTheme === 'true';
+    }
+    return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+  });
 
-    useEffect(() => {
-        if (isDarkMode) {
-            document.documentElement.setAttribute('data-theme', 'dark');
-        } else {
-            document.documentElement.removeAttribute('data-theme');
-        }
-        localStorage.setItem('dark-mode', isDarkMode.toString());
-    }, [isDarkMode]);
-
-    const toggleDarkMode = () => {
-        setIsDarkMode(prev => !prev);
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    const handleChange = (e: MediaQueryListEvent) => {
+      // Update the state only if there's no saved preference in localStorage
+      if (localStorage.getItem('dark-mode') === null) {
+        setIsDarkMode(e.matches);
+      }
     };
 
-    return (
-        <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
-            {children}
-        </DarkModeContext.Provider>
-    );
+    mediaQuery.addEventListener('change', handleChange);
+
+    // Set the initial theme
+    if (isDarkMode) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+      document.documentElement.removeAttribute('data-theme');
+    }
+
+    // Clean up listener on unmount
+    return () => mediaQuery.removeEventListener('change', handleChange);
+  }, [isDarkMode]);
+
+  const toggleDarkMode = () => {
+    const newIsDarkMode = !isDarkMode;
+    setIsDarkMode(newIsDarkMode);
+    localStorage.setItem('dark-mode', newIsDarkMode.toString());
+  };
+
+  return (
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
 };
 
 export const useDarkMode = (): DarkModeContextType => {
-    const context = useContext(DarkModeContext);
-    if (!context) {
-        throw new Error("useDarkMode must be used within a DarkModeProvider");
-    }
-    return context;
+  const context = useContext(DarkModeContext);
+  if (!context) {
+    throw new Error("useDarkMode must be used within a DarkModeProvider");
+  }
+  return context;
 };
